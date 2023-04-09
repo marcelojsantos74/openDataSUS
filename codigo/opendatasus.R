e735fb2b5eb59d5f29e7e96aeb8255d5d00db8df
marcelojsantos74 <marcelo.j.santos@unesp.br>
2023-04-09 20:29:51 -0300

    Desenvolvimento.

--- codigo/opendatasus.R
+++ codigo/opendatasus.R
@@ -15,7 +15,7 @@ library(readr)
 # Diretorio da Trabalho
 setwd('D:/GoogleDrive/Mestrado_unesp/openDataSUS')
 
-# Importacao dos arquivos anuais : 2021, 2022 e 2023 (2.339.381)
+# Importacao dos arquivos anuais : INFLUD* 2021, 2022 e 2023 (2.339.381)
 df_opendatasus<-list.files(path="./dados",pattern="*.csv", full.names = TRUE) %>%
   lapply(read.csv,stringsAsFactors=F, sep=';') %>%
   bind_rows
@@ -26,15 +26,10 @@ View(df_opendatasus)
 str(df_opendatasus) # apenas int e char
 summary(df)
 
-#Excluir colunas nao utilizadas
-df_opendatasus$SEM_NOT <- NULL
-df_opendatasus$DT_SIN_PRI <- NULL
+# Filtro - 78-Classificacao Final do caso (5-SRAG por COVID-19)
+df_covid <- df_opendatasus %>% filter(CLASSI_FIN == "5")
 
-str(df_opendatasus)
-df_opendatasus$
 
-# Filtro
-df %>%filter(CS_SEXO=="M")
 
 
 
